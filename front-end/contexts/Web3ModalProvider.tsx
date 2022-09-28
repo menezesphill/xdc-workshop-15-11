5cca0656c48a8dea14258b322818c8e54cbd94aa
Phillipe Menezes <phillipe.o.m@hotmail.com>
2022-09-28 16:28:58 -0300

    feat: added walletconnect option

--- front-end/contexts/Web3ModalProvider.tsx
+++ front-end/contexts/Web3ModalProvider.tsx
@@ -1,5 +1,8 @@
-import React, { createContext, useCallback, useState } from 'react';
-import Web3 from 'web3';
+import { createContext, useCallback, useEffect, useState } from "react";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 interface IWeb3ModalContext {
   web3: Web3 | null;
@@ -18,16 +21,59 @@ export const Web3ModalContext = createContext<IWeb3ModalContext>({
   account: null,
   chainId: null,
   networkId: null,
-  connected: false
+  connected: false,
 });
 
 const Web3ModalProvider = ({ children }) => {
-
+  const [web3Modal, setWeb3Modal] = useState<Web3Modal | null>(null);
   const [web3, setWeb3] = useState<Web3 | null>(null);
   const [account, setAccount] = useState<string | null>(null);
   const [chainId, setChainId] = useState<number | null>(null);
   const [networkId, setNetworkId] = useState<number | null>(null);
   const [connected, setConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    const providerOptions = {
+      "custom-xdc": {
+        display: {
+          name: "XDC Pay",
+          logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/2634.png",
+          description: "Connect with XDC Pay",
+        },
+        package: detectEthereumProvider,
+        connector: async (_detectEthereumProvider) => {
+          const provider = await _detectEthereumProvider();
+          console.log("provider", provider);
+          await provider.enable();
+          return provider;
+        },
+      },
+    };
+
+    const _web3Modal = new Web3Modal({
+      cacheProvider: true, // optional
+      providerOptions, // required
+      disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
+    });
+
+    setWeb3Modal(_web3Modal);
+  }, []);
+
+  const createWeb3 = (provider) => {
+    var realProvider;
+
+    if (typeof provider === "string") {
+      if (provider.includes("wss")) {
+        realProvider = new Web3.providers.WebsocketProvider(provider);
+      } else {
+        realProvider = new Web3.providers.HttpProvider(provider);
+      }
+    } else {
+      realProvider = provider;
+    }
+
+    return new Web3(realProvider);
+  };
 
   const resetWeb3 = useCallback(() => {
     setWeb3(null);
@@ -35,75 +81,45 @@ const Web3ModalProvider = ({ children }) => {
     setChainId(null);
     setNetworkId(null);
     setConnected(false);
-  }, [])
+  }, []);
 
-  const createWeb3 = (provider) => {
+  const subscribeProvider = useCallback(
+    async (_provider: any, _web3: Web3) => {
+      if (!_provider.on) return;
 
-    var realProvider;
-  
-    if (typeof provider === 'string') {
-      if (provider.includes('wss')) {
-        realProvider = new Web3.providers.WebsocketProvider(
-          provider
-        );
-      } else {
-        realProvider = new Web3.providers.HttpProvider(
-          provider
-        );
-      }
-    } else {
-      realProvider = provider;
-    }
-  
-    return new Web3(realProvider);
-  }
+      _provider.on("close", () => {
+        resetWeb3();
+      });
+      _provider.on("accountsChanged", async (accounts: string[]) => {
+        setAccount(_web3.utils.toChecksumAddress(accounts[0]));
+      });
+      _provider.on("chainChanged", async (chainId: number) => {
+        console.log("Chain changed: ", chainId);
+        const networkId = await _web3.eth.net.getId();
+        setChainId(Number(chainId));
+        setNetworkId(Number(networkId));
+      });
 
-  const subscribeProvider = useCallback(async (_provider: any, _web3: Web3) => {
-    if (!_provider.on)
-      return;
-
-    _provider.on("close", () => {
-      resetWeb3();
-    });
-
-    _provider.on("accountsChanged", async (accounts: string[]) => {
-      setAccount(_web3.utils.toChecksumAddress(accounts[0]));
-    });
-    
-    _provider.on("chainChanged", async (chainId: number) => {
-      const networkId = await _web3.eth.net.getId();
-      setChainId(Number(chainId));
-      setNetworkId(Number(networkId));
-    });
-
-    _provider.on("networkChanged", async (networkId: number) => {
-      const chainId = await _web3.eth.getChainId();
-      setChainId(Number(chainId));
-      setNetworkId(Number(networkId));
-    });
-  }, [resetWeb3])
+      _provider.on("networkChanged", async (networkId: number) => {
+        const chainId = await _web3.eth.getChainId();
+        setChainId(Number(chainId));
+        setNetworkId(Number(networkId));
+      });
+    },
+    [resetWeb3]
+  );
 
   const connect = useCallback(async () => {
+    if (!web3Modal) return;
 
-    if(!globalThis.ethereum) return;
+    const _provider = await web3Modal.connect();
+    if (_provider === null) return;
 
-    const _provider = globalThis.ethereum.isXDCPay ? globalThis.web3.currentProvider : globalThis.ethereum;
-
-    try{
-      await globalThis.ethereum.request({ method: "eth_requestAccounts" });
-    } catch (e) {
-      console.log("eth_requestAccounts is not a XDCPay feature!");
-    }
-    
-    
-    if (_provider === null) 
-      return;
-    
     const _web3 = createWeb3(_provider);
     setWeb3(_web3);
 
     await subscribeProvider(_provider, _web3);
-    
+
     const accounts = await _web3.eth.getAccounts();
     const _account = _web3.utils.toChecksumAddress(accounts[0]);
     const _networkId = await _web3.eth.net.getId();
@@ -113,39 +129,40 @@ const Web3ModalProvider = ({ children }) => {
     setNetworkId(Number(_networkId));
     setChainId(Number(_chainId));
     setConnected(true);
+  }, [web3Modal, subscribeProvider]);
 
-    console.log(_account)
-    console.log(_networkId)
-    console.log(_chainId)
-    console.log(connected)
-    console.log(_web3)
-    
-  }, [subscribeProvider]);
-
+  useEffect(() => {
+    if (web3Modal && web3Modal.cachedProvider) {
+      connect();
+    }
+  }, [web3Modal, connect]);
 
   const disconnect = useCallback(async () => {
     if (web3 && web3.currentProvider) {
       const _provider: any = web3.currentProvider;
-      if (_provider.close)
-        await _provider.close();
+      if (_provider.close) await _provider.close();
+    }
+    if (web3Modal) {
+      await web3Modal.clearCachedProvider();
     }
     resetWeb3();
-  }, [web3, resetWeb3])
+  }, [web3Modal, web3, resetWeb3]);
 
   return (
-    <Web3ModalContext.Provider 
-      value={{ 
-        web3, 
+    <Web3ModalContext.Provider
+      value={{
+        web3,
         connect,
         disconnect,
         account,
         networkId,
         chainId,
-        connected
-      }}>
+        connected,
+      }}
+    >
       {children}
     </Web3ModalContext.Provider>
-  )
-}
+  );
+};
 
 export default Web3ModalProvider;
