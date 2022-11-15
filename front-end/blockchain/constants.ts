5b70ad04ac804beb19d51b78b643f23a1a0ee1c0
Phill Menezes <phillipe.o.m@hotmail.com>
2022-11-15 19:57:28 -0300

    Update constants.ts

--- front-end/blockchain/constants.ts
+++ front-end/blockchain/constants.ts
@@ -9,9 +9,3 @@ export const stakingAddress = {
     51: '0x037C45dEa764DAa791A4A37825Ae49D82fB53E4e',
   },
 }
-
-export const faucetAddress = {
-  Faucet: {
-    51: '0x7a93d460870e354E1274e7d1f59c37305a6AcD4f',
-  },
-}
